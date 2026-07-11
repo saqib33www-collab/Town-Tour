f8633f633284b2762edb2fa34521758055e74f92
saqib <saqib33www@gmail.com>
2026-07-11 16:14:24 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+// Syntax: for (initialization; condition; update) { ... }
+for (let i = 1; i <= 3; i++) {
+  console.log("Iteration number: " + i);
+}
+
+// Output:
+// Iteration number: 1
+// Iteration number: 2
+// Iteration number: 3
+
